3603b7ef2f1ecc6ba5bb234e8d51d1952c7cad11
Md. Asaduzzaman Shuvo <100297947+ashuvo25@users.noreply.github.com>
2023-11-01 18:20:52 +0600

    Update copycopycopy.cpp

--- copycopycopy.cpp
+++ copycopycopy.cpp
@@ -3,27 +3,20 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-
 #define ll long long
-
 int main()
 {
  int t;cin>>t;
      while (t--){
         int n;cin>>n;
         set<int> s;
-
         for (int  i = 0; i < n; i++)
         {
             int x;
             cin >> x;
             s.insert(x);
         }
-        
-
         cout<< s.size()<<endl;
-
      }
-  
     return 0;
 }
