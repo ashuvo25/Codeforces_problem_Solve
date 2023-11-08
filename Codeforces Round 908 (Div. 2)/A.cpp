90ce996b02b4a889c2d03f0e798ddc4ec2f690e5
Md. Asaduzzaman Shuvo <100297947+ashuvo25@users.noreply.github.com>
2023-11-09 00:36:52 +0600

    Add files via upload

---
+++ Codeforces Round 908 (Div. 2)/A.cpp
@@ -0,0 +1,22 @@
+ /*******************বিসমিল্লাহির রাহমানির রাহিম **********************/
+ /*__________________ MD ASADUZZAMAN SHUVO ______________________ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int INF = 1e9+10;
+const int N = 1e5+10;
+
+int main()
+{
+ int t;cin>>t;
+  while(t--){
+    ll n;cin>>n;
+    string s;cin >>s;
+    if(s[s.size() -1] == 'A') cout << "A" <<endl;
+    else cout<< "B" <<endl;
+  }
+
+    return 0;
+}
