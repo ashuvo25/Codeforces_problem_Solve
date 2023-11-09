a1e1c41d77cb5c91642a62b4c6a801a90ca4340f
Md. Asaduzzaman Shuvo <100297947+ashuvo25@users.noreply.github.com>
2023-11-09 19:42:39 +0600

    Update A-Winner.cpp

--- A-Winner.cpp
+++ A-Winner.cpp
@@ -11,7 +11,6 @@ int main()
  int t;cin>>t;
    map<string,int> m;
    vector<pair<string,int>> m1;
-  // int maxi = 0;
     for (int i = 0; i < t; i++)
     { 
        string s;cin>>s;
@@ -20,24 +19,10 @@ int main()
        m1.push_back(make_pair(s,m[s]));
     }
     int maxi = INT_MIN;
-
     for(auto &it : m){
         maxi = max(maxi,it.second);
     }
-//map<string,int> mm;
 string winner = "";
-//map<string,int> :: iterator it1;
-// int i = 0;
-//  for(auto &it: m1){
-    // string q =  it.first;
-    // int w = it.second;
-  //  mm[q] += w;
-        
-        // if(maxi == m1[i].second){
-        // winner=winner+m1[i].first;
-        // break;
-        // }
-//}
  for (int i = 0; i < t; i++)
  {
     pair<string,int> k = m1[i];
@@ -45,9 +30,6 @@ string winner = "";
         cout << k.first <<endl;
         break;
     }
- }
- 
- //cout<< maxi <<endl;
-    
+ }  
     return 0;
 }
