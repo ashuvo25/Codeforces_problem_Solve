a4dd6c4c883cbd829314f6493e7abe01affe4383
Md. Asaduzzaman Shuvo <100297947+ashuvo25@users.noreply.github.com>
2023-11-09 19:41:24 +0600

    Update E1.cpp

--- Codeforces Round 883 (Div. 3)/E1.cpp
+++ Codeforces Round 883 (Div. 3)/E1.cpp
@@ -5,14 +5,12 @@
 using namespace std;
 
 #define ll long long
-
 int main()
 {
  int t;cin>>t;
   while(t--){
     ll n;cin>> n;
     int  q = 0;
-
     for(int i = 2;i<sqrt(n);i++){
      int w = 1;
         while ( w <= n){
@@ -23,6 +21,5 @@ int main()
    if(q == 0) cout<< "NO" <<endl;
    else cout<< "YES" <<endl;
   }
-
     return 0;
 }
