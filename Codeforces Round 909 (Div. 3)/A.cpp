15c802e448e3173b9c4ff2cecdb54ae5b84711d1
Md. Asaduzzaman Shuvo <100297947+ashuvo25@users.noreply.github.com>
2023-11-18 08:14:49 +0600

    Add files via upload

---
+++ Codeforces Round 909 (Div. 3)/A.cpp
@@ -0,0 +1,23 @@
+ /*******************বিসমিল্লাহির রাহমানির রাহিম **********************/
+ /*__________________ MD ASADUZZAMAN SHUVO ______________________ */
+
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+const int INF = 1e9+10;
+const int N = 1e5+10;
+
+int main()
+{
+ int t;cin>>t;
+while(t--){
+    ll n;cin>>n;
+    if(n%3 !=0 )cout<< "First"<<endl;
+     else{
+        cout << "Second"<<endl;
+     }
+}
+
+    return 0;
+}
